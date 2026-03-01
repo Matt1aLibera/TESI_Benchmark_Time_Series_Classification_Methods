1f088f9f4edc74b8494dfcd546e3efe2c95293d8
Mattia Liberatore <mattia.liberatore@edu.unito.it>
2026-03-01 21:57:52 +0100

    hc2 elec dev di nuovo

--- skTimeTest/benchmark_all_algorithms_parallel.py
+++ skTimeTest/benchmark_all_algorithms_parallel.py
@@ -29,32 +29,39 @@ SEEDS: List[int] = [0, 1, 2]
 
 # Lista dei dataset (puoi decommentare quelli che vuoi far girare stanotte)
 DATASET_NAMES: List[str] = [
-    "InsectEPGSmallTrain",
-    "ItalyPowerDemand",
-    "SmoothSubspace",#
-    "Fungi",
-    "DiatomSizeReduction",
-    "Chinatown",
-    "HouseTwenty",
-    "Rock",
-    "HandOutlines",
-    "Crop",
-    "FordB",
+    #"InsectEPGSmallTrain",
+    #"ItalyPowerDemand",
+    #"SmoothSubspace",#
+    #"Fungi",
+    #"DiatomSizeReduction",
+    #"Chinatown",
+    #"HouseTwenty",
+    #"Rock",
+    #"HandOutlines",
+    #"Crop",
+    #"FordB",
     "ElectricDevices",
 ]
 
 ALGORITHMS_TO_RUN = [
     {
-        "name": DRCIF_NAME,
-        "variant": "DrCIF_Standard",
-        "params": {"n_estimators": 200, "att_subsample_size": 10, "time_limit_in_minutes": 180}
-    },
-    {
-        "name": STC_NAME,
-        "variant": "STC_Standard",
+        "name": HC2_NAME,
+        "variant": "HC2_Standard_Test",
         "params": {
-            "n_shapelet_samples": 10000, "n_estimators": 200,
-            "max_shapelets": None, "time_limit_in_minutes": 120}
+            "stc_params": {
+                "n_shapelet_samples": 10000,
+                "max_shapelets": None,
+                "time_limit_in_minutes": 120  # Limite manuale per STC
+            },
+            "drcif_params": {
+                "n_estimators": 200,
+                "att_subsample_size": 10,
+                "time_limit_in_minutes": 180  # Limite manuale per DrCIF
+            },
+            "arsenal_params": {"num_kernels": 2000, "n_estimators": 25},
+            "tde_params": {"n_parameter_samples": 250, "max_ensemble_size": 50, "time_limit_in_minutes": 180},
+            # Limite manuale per TDE
+        }
     },
 ]
 
@@ -151,7 +158,7 @@ if __name__ == "__main__":
 
     all_benchmark_results = []
     # Definiamo il nome del file CSV per il salvataggio incrementale
-    csv_filename = "benchmark_results_FAST_STC&DRCIF.csv"
+    csv_filename = "benchmark_results_SLOW4.csv"
     # --- PULIZIA AUTOMATICA ---
     if os.path.exists(csv_filename):
         print(f"Rilevato vecchio file {csv_filename}. Rimozione in corso per nuova run...")
